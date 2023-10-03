44753875863a84d62c8afc0d17a0d82ffa446a74
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2023-10-04 00:06:31 +0800

    Do not install gdb from apt

--- setup.sh
+++ setup.sh
@@ -20,7 +20,9 @@ osx() {
 
 install_apt() {
     sudo apt-get update || true
-    sudo apt-get install -y git gdb gdbserver python3-dev python3-venv python3-pip python3-setuptools libglib2.0-dev libc6-dbg
+    # Do not install gdb and gdbserver from apt, they are too old
+    # sudo apt-get install -y git gdb gdbserver python3-dev python3-venv python3-pip python3-setuptools libglib2.0-dev libc6-dbg
+    sudo apt-get install -y git python3-dev python3-venv python3-pip python3-setuptools libglib2.0-dev libc6-dbg
 
     if uname -m | grep x86_64 > /dev/null; then
         sudo dpkg --add-architecture i386 || true
